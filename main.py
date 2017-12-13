86c705bf7c1771c9ed0cc94ce20755a60c1e6670
jamieh <j.hayes@cs.ucl.ac.uk>
2017-12-13 14:19:03 +0000

    dataloader loading twice fix

--- main.py
+++ main.py
@@ -151,11 +151,6 @@ if opt.dataset == 'cifar10':
     testset = dset.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=opt.batchSize, shuffle=False, num_workers=2)
     
-    trainset = dset.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=opt.batchSize, shuffle=True, num_workers=2)
-    testset = dset.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=opt.batchSize, shuffle=False, num_workers=2)
-
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
