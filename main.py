c187a446043c13e1da1c17beb93c4f7f9792e8e7
jamieh <j.hayes@cs.ucl.ac.uk>
2017-12-13 14:07:43 +0000

    fixed dataloader scale problem

--- main.py
+++ main.py
@@ -136,14 +136,14 @@ if opt.cuda:
 print('==> Preparing data..')
 if opt.dataset == 'cifar10':
     transform_train = transforms.Compose([
-        transforms.ToTensor(),
         transforms.Scale((opt.imageSize,opt.imageSize)),
+        transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     
     transform_test = transforms.Compose([
-        transforms.ToTensor(),
         transforms.Scale((opt.imageSize,opt.imageSize)),
+        transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     trainset = dset.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
